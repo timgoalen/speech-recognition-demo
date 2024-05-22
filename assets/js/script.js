02dd350861732c09a5dc4a7868637234713884f7
Tim Goalen <timgoalen@hotmail.com>
2024-05-22 14:39:03 +0000

    feat: implement speech recognition api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,69 @@
-// -- Get HTML elements
+// -- GET HTML ELEMENTS --
 
 const mainBtn = document.getElementById("main-btn");
 const listeningAlertContainer = document.getElementById(
   "listening-alert-container"
 );
 
-// -- Click handlers
+function initSpeechRecognition() {
+  let recognising = false;
+  let masterTranscript = "";
 
-function handleMainBtnClick() {
-  console.log("clicked");
+  const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+  if (!SpeechRecognition) {
+    alert(
+      "Sorry, your browser does not support Speech Recognition. Try using Chrome or Safari."
+    );
+    return;
+  }
 
-  mainBtn.innerText === "Listen"
-    ? (mainBtn.innerText = "Stop + Send")
-    : (mainBtn.innerText = "Listen");
+  const recognition = new SpeechRecognition();
 
-  listeningAlertContainer.classList.toggle("show-element");
+  // Recognition settings
+
+  recognition.continuous = true;
+  recognition.interimResults = false;
+  recognition.lang = "en-UK";
+  recognition.onresult = (event) => {
+    let currentTranscript = "";
+    for (let i = event.resultIndex; i < event.results.length; i++) {
+      currentTranscript = event.results[i][0].transcript;
+    }
+    masterTranscript += currentTranscript;
+  };
+  recognition.onerror = function (event) {
+    console.error("Speech recognition error: " + event.error);
+  };
+
+  function handleMainBtnClick() {
+    // Toggle the button text
+    mainBtn.innerText === "Listen"
+      ? (mainBtn.innerText = "Stop + Send")
+      : (mainBtn.innerText = "Listen");
+
+    // Display "Listening.." in UI
+    listeningAlertContainer.classList.toggle("show-element");
+
+    // Start/stop the Speech Recognition
+    if (recognising) {
+      recognition.stop();
+      recognising = false;
+      console.log("Master:", masterTranscript);
+      console.log("Speech recognition stopped.");
+      masterTranscript = "";
+    } else {
+      recognition.start();
+      recognising = true;
+      console.log("Speech recognition started.");
+    }
+  }
+
+  mainBtn.addEventListener("click", handleMainBtnClick);
 }
 
-// -- Event listeners
+// -- CLICK HANDLERS --
 
-mainBtn.addEventListener("click", handleMainBtnClick);
+// -- EVENT LISTENERS --
+
+window.onload = initSpeechRecognition;
